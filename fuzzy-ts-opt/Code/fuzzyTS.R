612799da0e209a33d363b410435163f40c8971ec
jairpaulino <63611561+jairpaulino@users.noreply.github.com>
2020-05-28 12:35:16 -0300

    First version

---
+++ fuzzy-ts-opt/Code/fuzzyTS.R
@@ -0,0 +1,72 @@
+# Cria funcao fitness
+fitnessGA = function(C, n, w, pos_type, time_series = train.set){
+  #time_series = train.set; C = 0.7; n = 17.1; w = 5.3; pos_type = 1.2
+  
+  C = round(C, 0); n = round(n, 0)
+  w = round(w, 0); pos_type = round(pos_type, 0)
+  type_alg = c("Abbasov-Mamedova","NFTS")
+  fuzzy1 = fuzzy.ts2(as.ts(time_series), 
+                     C = C, n = n, w = w, type = type_alg[pos_type], 
+                     forecast = 1)  
+  
+  matriz.previsao = as.data.frame(matrix(nrow = length(fuzzy1$interpolate), ncol = 2))
+  names(matriz.previsao) = c("obs", "forecast")
+  matriz.previsao$obs = time_series
+  matriz.previsao$forecast = fuzzy1$interpolate 
+  matriz.previsao = na.omit(matriz.previsao)
+  return(getMSE(matriz.previsao$obs, matriz.previsao$forecast))  
+}
+
+getOptGAParameters = function(){
+
+  # c() - C, n, w, pos_type
+  lower = c(0, 02, 02, 1)
+  upper = c(1, 50, 12, 2)
+  GA <- ga(type = "real-valued", 
+           fitness =  function(x) -fitnessGA (x[1], x[2], x[3], x[4]),
+           lower = lower, upper = upper, 
+           pcrossover = 0.9,
+           pmutation = 0.1,
+           popSize = 30,
+           maxiter = 30, 
+           seed = 123)
+  plot(GA)
+  C = summary(GA)$solution[1]; n = round(summary(GA)$solution[2]) 
+  w = round(summary(GA)$solution[3]); pos_type = round(summary(GA)$solution[4]) 
+  result = c(C, n, w, pos_type)
+  return(result)
+}
+
+# Calcula o modelo FuzzyTS
+getFuzzyTS = function(time_series, GAParameters){ #time_series = AirPassengers
+
+  type_alg = c("Abbasov-Mamedova","NFTS")
+  fuzzy1 = fuzzy.ts2(as.ts(time_series), 
+                     C = GAParameters[1],
+                     n = GAParameters[2],
+                     w = GAParameters[3],  
+                     type = type_alg[GAParameters[4]],
+                     forecast = 1)
+  return(fuzzy1$forecast)
+}
+
+# Calcula o valor 1-Step ahead
+get1StepAheadFuzzyTS = function(train.set, test.set, GAParameters){
+  
+  time_series_all = train.set
+  fts_forecast_all = NULL
+  forecast_number = length(test.set)
+  
+  c=1
+  for (i in 1:forecast_number){
+   
+    fts_forecast = getFuzzyTS(time_series_all, GAParameters) #cria modelo
+    fts_forecast_all[i] = as.numeric(fts_forecast) #transforma a previsão em valor númérico
+    time_series_all = c(time_series_all, test.set[i]) #atualiza o vetor utilizado no calculo da previsao 
+    print(c)
+    c=c+1
+  
+  }
+  
+  return(fts_forecast_all)
+}
---
+++ fuzzy-ts-opt/Code/mainFuzzyTSOpt.R
@@ -0,0 +1,100 @@
+rm(list = ls()) # Limpando dados
+
+# Selecionando diretorio de trabalho
+#setwd("C:/Users/jairp/Dropbox/_Papers_Books/01_Artigos_em_andamento/Eduardo/Fuzzy/")
+#setwd('C:/Users/Eduardo/OneDrive - Instituto Leao Sampaio de Ensino Universitario Ltda/Artigos_2020/Banco de dados/Data_Tese/Fuzzy') 
+
+# Bibliotecas requeridas
+library(forecast)
+library(AnalyzeTS) #Analisa s?ries temporais fuzzy STF
+library(GA)
+
+# Chamando fun??es externas
+source("Code/preProcessing.R")
+source("Code/fuzzyTS.R")
+source("Code/performanceMetrics.R")
+source("Code/optimalArima.R")
+
+# Lendo dados
+# MATAL; PIPVI; POPAZ
+dados = read.csv("Data/LYNX.csv", sep = ";"); head(dados)
+
+# Criando conjuntos de treinamento e teste
+tamanho_dados = length(dados$target)
+percentual_train = 0.8
+#n = round(length(percentual_train)) corrigir o bug
+train.set = dados$target[1:round((tamanho_dados*percentual_train))] #plot.ts(train.set)
+test.set = dados$target[round((tamanho_dados*percentual_train+1)):tamanho_dados] #plot.ts(test.set)
+
+# Calcula previsao 1-Step ahed para o modelo FTS
+begin_fuzzy = proc.time()
+GAParameters = getOptGAParameters()
+fuzzy_forecast = get1StepAheadFuzzyTS(train.set, test.set, GAParameters)
+end_fuzzy = proc.time(); tempo_proc_fuzzy = end_fuzzy - begin_fuzzy
+
+# Cria modelo arima e realizar previsao
+begin_arima = proc.time()
+arima_model = getOptimalARIMA(train.set)
+arima_forecast = getARIMAForecasts(test.set, arima_model)
+end_arima = proc.time(); tempo_proc_arima= end_arima - begin_arima
+
+# Cria modelo ets e realizar previsao
+begin_ets = proc.time()
+ets_model = getOptimalETS(train.set)
+ets_forecast = getETSForecasts(test.set, ets_model)
+end_ets = proc.time(); tempo_proc_ets= end_ets - begin_ets
+
+# Cria modelo ann e realizar previsao
+begin_ann = proc.time()
+ann_model = getOptimalANN(train.set)
+ann_forecast = getANNForecasts(test.set, ann_model)
+end_ann = proc.time(); tempo_proc_ann= end_ann - begin_ann; tempo_proc_ann
+
+# Cria tabela com resultados
+result_models = as.data.frame(matrix(nrow = length(test.set), ncol = 5))
+names(result_models) = c("obs", "fuzzy", "arima", "ets", "ann") 
+result_models$obs = test.set
+result_models$fuzzy = fuzzy_forecast
+result_models$arima = arima_forecast
+result_models$ets = ets_forecast
+result_models$ann = ann_forecast
+#result_models = na.omit(result_models)
+
+result_models_metrics = as.data.frame(matrix(nrow = 3, ncol = 4))
+names(result_models_metrics) = c("fuzzy", "arima", "ets", "ann") 
+rownames(result_models_metrics) = c("MSE", "MAPE", "Theils' U")
+
+result_models_metrics[1, 1] = getMSE(result_models$obs, result_models$fuzzy)
+result_models_metrics[2, 1] = getMAPE(result_models$obs, result_models$fuzzy)
+result_models_metrics[3, 1] = getTheil(result_models$obs, result_models$fuzzy)
+
+result_models_metrics[1, 2] = getMSE(result_models$obs, result_models$arima)
+result_models_metrics[2, 2] = getMAPE(result_models$obs, result_models$arima)
+result_models_metrics[3, 2] = getTheil(result_models$obs, result_models$arima)
+
+result_models_metrics[1, 3] = getMSE(result_models$obs, result_models$ets)
+result_models_metrics[2, 3] = getMAPE(result_models$obs, result_models$ets)
+result_models_metrics[3, 3] = getTheil(result_models$obs, result_models$ets)
+
+result_models_metrics[1, 4] = getMSE(result_models$obs, result_models$ann)
+result_models_metrics[2, 4] = getMAPE(result_models$obs, result_models$ann)
+result_models_metrics[3, 4] = getTheil(result_models$obs, result_models$ann)
+result_models_metrics
+
+# Cria grafico 
+plot.ts(test.set, lwd = 2)
+lines(fuzzy_forecast, lwd = 2, col = 2)
+lines(arima_forecast, lwd = 2, col = 3)
+lines(ets_forecast, lwd = 2, col = 4)
+lines(ann_forecast, lwd = 2, col = 5)
+
+options(scipen=999)
+result_models_metrics
+
+result_models_time = as.data.frame(matrix(nrow = 1, ncol = 4))
+names(result_models_time) = c("fuzzy", "arima", "ets", "ann") 
+result_models_time$fuzzy = tempo_proc_fuzzy[3]
+result_models_time$arima = tempo_proc_arima[3]
+result_models_time$ets = tempo_proc_ets[3]
+result_models_time$ann = tempo_proc_ann[3]
+result_models_time
