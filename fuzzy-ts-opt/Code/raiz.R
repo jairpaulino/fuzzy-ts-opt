23a32edba485fcb77603e1d6b9c0a890dc81cc4f
jairpaulino <63611561+jairpaulino@users.noreply.github.com>
2020-07-08 16:18:15 -0300

    Update raiz.R

--- fuzzy-ts-opt/Code/raiz.R
+++ fuzzy-ts-opt/Code/raiz.R
@@ -2,7 +2,7 @@ rm(list=ls())
 
 source("Code/funcaoFTS.R")
 
-dados = read.csv("Data/POPAZ.csv", sep = ";"); head(dados, 5)
+dados = read.csv("Data/PIBPC.csv", sep = ";"); head(dados, 5)
 
 fuzzy = getfuzzification(dados, D1 = 1800, D2 = 1100, n = 7, C = 0.0001)
 #View(fuzzy)
