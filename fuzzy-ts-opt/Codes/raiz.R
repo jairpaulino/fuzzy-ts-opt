d5a37c82f906933ee1ee77eccd4d16b8296a9f5f
jairpaulino <63611561+jairpaulino@users.noreply.github.com>
2020-07-10 17:39:14 -0300

    Update raiz.R

--- fuzzy-ts-opt/Codes/raiz.R
+++ fuzzy-ts-opt/Codes/raiz.R
@@ -2,18 +2,19 @@ rm(list=ls())
 
 source("Codes/funcoesFTS.R")
 
-#POPAZ, PIBVI
+#POPAZ, PIBVI, IGPOG
 dados = read.csv("Data/POPAZ.csv", sep = ";"); tail(dados, 5)
 
-D1 = 1800; D2 = 1100; n = 7; C = 0.0001; w = 7
+D1 = 1800; D2 = 1100; n = 4; C = 0.0001; w = 4 #parametros
 
-fuzzy = getfuzzification(dados, D1 = D1, D2 = D2, n = n, C =C)
-fuzzy
+fuzzy = getFuzzification(dados, D1 = D1, D2 = D2, n = n, C = C); fuzzy
 
-rm = getRelationsMatrix(matrizFuzzy = fuzzy$matrizFuzzy, time.series = dados$target,
-                        w = w, n = n) 
-rm
+rm = getRelationsMatrix(matrixFuzzy = fuzzy$matrixFuzzy, time.series = dados$target,
+                        w = w, n = n); rm
 
-def = getDefuzzificationAndForecasting(R = rm$R, K = rm$K, n = n, 
-                                    uim = fuzzy$middlePoint, timeSeries = dados)
-def
+defForecast = getDefuzzificationAndForecasting(R = rm$R, K = rm$K, n = n, 
+                                    uim = fuzzy$middlePoint, timeSeries = dados$target); defForecast
+
+# analisar o numero de diferenciacoes a partir de 2
+# criar grafico de previsao
+# criar funcao que passa o modelo com parametro
