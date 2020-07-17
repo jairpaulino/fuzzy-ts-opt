6a12aa3ee2beb87accc562125fa4786a6614eefe
jairpaulino <63611561+jairpaulino@users.noreply.github.com>
2020-07-17 14:52:16 -0300

    Update funcoesFTS.R

--- fuzzy-ts-opt/Codes/funcoesFTS.R
+++ fuzzy-ts-opt/Codes/funcoesFTS.R
@@ -1,7 +1,7 @@
 # Step 1 - Differentiation and Discourse Universe
 getDiscourseUniverse = function(time.series, D1, D2, n){ 
-  #time.series=dados; D1=1800; D2=1100; n=7; C=0.0001
-  ts.diff <- as.vector(diff(time.series$target))
+  #time.series=dados$target; D1=1800; D2=1100; n=7; C=0.0001
+  ts.diff <- as.vector(diff(time.series))
   Vmin <- min(ts.diff) - D1
   Vmax <- max(ts.diff) + D2 
   h <- (Vmax - Vmin)/n 
@@ -30,13 +30,13 @@ getFuzzification = function(time.series, D1, D2, n, C){
   
   discourseUniverse = getDiscourseUniverse(time.series, D1, D2, n)
   
-  time = min(time.series$time):max(time.series$time)
+  #time = min(time.series$time):max(time.series$time)
   Ai = 1:length(discourseUniverse$diff)
   matrixFuzzy <- matrix(1:(length(discourseUniverse$diff)*n), ncol = n) 
   for (i in 1:(length(discourseUniverse$diff))) {
      for (j in 1:n)
       matrixFuzzy[i,j] <- 1/(1 + (C * (discourseUniverse$diff[i] - discourseUniverse$discourseUniverse[j, 4]))^2) #Gerando a matriz com os valores de associacao de cada variacao
-      rownames(matrixFuzzy) = as.numeric(c((min(time.series$time)+1):max(time.series$time))) 
+      #rownames(matrixFuzzy) = as.numeric(c((min(time.series$time)+1):max(time.series$time))) 
       colnames(matrixFuzzy) = discourseUniverse$U
   }
   
@@ -78,7 +78,7 @@ getRelationsMatrix = function(matrixFuzzy, time.series, w, n){
   #return(list(R, K))
 }
 
-#Step 4 - Deffuzification of fuzzy values
+#Step xx - 
 getDefuzzificationAndForecasting = function(R, K, n, uim, timeSeries){
   Vi <- 1:n #vetor de numeros inteiros
   Ft <- NULL #K[[2]] #F(t) valor previsto para o ano t de forma difusa
@@ -89,5 +89,70 @@ getDefuzzificationAndForecasting = function(R, K, n, uim, timeSeries){
   #Vi <- round(Vi, 0)
   #print(Ft)
   forecast = timeSeries[length(timeSeries)] + Vi
+  return(forecast)
+}
+
+getRelationsMatrixOneStepAhead = function(matrixFuzzy, w, n){
+  #w = 7; i = 7
+  #matrixFuzzy = matrixFuzzyOneStep
+  matrixFuzzy = as.data.frame(matrixFuzzy)
+  tamanhoObservacoes = length(matrixFuzzy$u1)
+  
+  O = matrixFuzzy[(tamanhoObservacoes-w+1):(tamanhoObservacoes-1),] #matriz de operacoes
+  K = matrixFuzzy[(tamanhoObservacoes),] #matriz de criterios
+  R = O #matriz de relacoes nebulosas
+  for (i in 1:(w-1)){ #i=1; j=1
+    for (j in 1:n){
+      if(K[j] < O[i,j]){
+        R[i,j] = K[1, j] 
+      }else{
+        R[i,j] = O[i,j]
+      }
+    }
+  }
+  
+  #write.csv2(table4, file = "RelationsMatrix", append = FALSE, sep = "\t", dec = ".",
+  #row.names = TRUE, col.names = TRUE)
+  RM = NULL
+  RM$O = O
+  RM$K = K
+  RM$R = R
+  return(RM)
+  #return(list(R, K))
+}
+
+getDefuzzificationAndForecastingOneStep = function(R, K, n, uim, timeSeries, pos){
+  #R = rm$R; K = rm$K; n = n; uim =  model$middlePoint; timeSeries = dados$target
+  Vi <- 1:n #vetor de numeros inteiros
+  Ft <- NULL #K[[2]] #F(t) valor previsto para o ano t de forma difusa
+  for (j in 1:n){
+    Ft[j] <- max(R[,j])
+  }
+  Vi <- sum((Ft * uim)/sum(Ft)) #funcao de defuzzificacao
+  #Vi <- round(Vi, 0)
+  #print(Ft)
+  forecast = timeSeries[pos+1] + Vi
+  return(forecast)
+}
+
+oneStepAheadForecasting = function(time.series, D1, D2, n, w, C){ 
+  #time.series = dados$target; D1 = 1800; D2 = 1100; n = 7; w = 7; C = 0.1
+  model = getFuzzification(time.series, D1, D2, n, C)
+  
+  forecast = NULL
+  for (i in w:(length(time.series)-1)){ #i=8
+    
+    matrixFuzzyOneStep = model$matrixFuzzy[(i-w+1):(i),]
+    
+    rm = getRelationsMatrixOneStepAhead(matrixFuzzy = matrixFuzzyOneStep, 
+                                        w = w, n = n)
+    
+    forecast[i+1] = getDefuzzificationAndForecastingOneStep(rm$R, rm$K, n = n, 
+                                                model$middlePoint, timeSeries = time.series, 
+                                                pos = i)
+  }
+  
+  plot.ts(time.series)
+  lines(forecast, col = 2, lwd = 2)
   return(forecast)
 }
