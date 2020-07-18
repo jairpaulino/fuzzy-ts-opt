024f941b28a5df7ccf1dabe569a055885c25d454
jairpaulino <63611561+jairpaulino@users.noreply.github.com>
2020-07-17 23:14:31 -0300

    Update raiz.R

--- fuzzy-ts-opt/Codes/raiz.R
+++ fuzzy-ts-opt/Codes/raiz.R
@@ -1,39 +1,56 @@
 rm(list=ls())
 
-source("Codes/funcoesFTS.R")
+#install.packages("GA", dependencies = T)
+library(GA)
+#install.packages("forecast", dependencies = T)
+library(forecast) #ARIMA, ETS e NNETAR
+
+#source("Codes/funcoesFTS.R")
+source("Codes/fuzzyTSOptGA_SA.R")
 source("Codes/performanceMetrics.R")
+source("Codes/optimalArimaETS.R")
 
-#POPAZ, PIBVI, IGPOG, ONI
-dados = read.csv("Data/DEF2.csv", sep = ";"); tail(dados, 5)
+# PIBBV, LYNX, 
+# DEF, PIBPC, MATAL 
+# MUC, POPAZ, IGPOG, SUNY
+dados = read.csv("Data/ONI.csv", sep = ";"); tail(dados, 5)
 
-D1 = 1800; D2 = 1100; n = 4; C = 0.0001; w = 4 #parametros
+data_train = dados$target[1:round((length(dados$target)*0.75))]
+data_test = dados$target[(round((length(dados$target)*0.75))+1):length(dados$target)]
 
-fuzzy = getFuzzification(dados$target, D1 = D1, D2 = D2, n = n, C = C); fuzzy
+# Phase 02 - Training phase (modelling) #####
+# Get optimal ARIMA and ANN models, respectively
+arima_model = getOptimalARIMA(data_train)
+ets_model = getOptimalETS(data_train)
+nnar_model = getOptimalNNAR(data_train)
 
-rm = getRelationsMatrix(matrixFuzzy = fuzzy$matrixFuzzy, time.series = dados$target,
-                        w = w, n = n); rm
-
-defForecast = getDefuzzificationAndForecasting(R = rm$R, K = rm$K, n = n, 
-                                    uim = fuzzy$middlePoint, timeSeries = dados$target); defForecast
-#IGPOG
-#time.series = dados$target; D1 = 1800; D2 = 1100; n = 7; w = 7; C = 0.0001
-#time.series = dados$target; D1 = 1800; D2 = 1100; n = 15; w = 5; C = 0.1
-#time.series = dados$target; D1 = 1800; D2 = 1100; n = 15; w = 5; C = 0.01
-#time.series = dados$target; D1 = min(dados$target*0.80); D2 = max(dados$target*1.2); n = 5; w = 10; C = 0.001
-time.series = dados$target; D1 = 0; D2 = 0; n = 30; w = 2; C = 0.00001
-
-TESTE = oneStepAheadForecasting(time.series = time.series,
-                           D1 = D1, D2 = D2, n = n, w = w, C = C)
-
-getMAPE(TESTE, time.series)
-getARV(TESTE, time.series)
+# Phase 03 - Test phase (forecasting) #####
+# One-step ahead approach#
+onestep_arima = getARIMAForecasts(data_test, arima_model)
+onestep_ets = getETSForecasts(data_test, model = ets_model)
+onestep_nnar = getNNARForecasts(data_test, model = nnar_model)
 
 
+gaParameters = getOptGAParameters(data_train)
+teste = oneStepAheadForecasting(time.series = data_test,
+                                D1 = as.numeric(gaParameters[1]),
+                                D2 = as.numeric(gaParameters[2]),
+                                C = as.numeric(gaParameters[3]),
+                                n = as.numeric(gaParameters[4]),
+                               w = as.numeric(gaParameters[5]))
 
-# calcular delta:
-#  D1 = D2 = 30% - Amplitude 
+getARV(teste, data_test) #FUZZY
+getARV(onestep_arima, data_test) #ARIMA
+getARV(onestep_ets, data_test) #ETS
+getARV(onestep_nnar, data_test) #NNETAR
 
-#0.008
-# analisar o numero de diferenciacoes a partir de 2
-# criar grafico de previsao
-# criar funcao que passa o modelo com parametro
+getMSE(teste, data_test) #FUZZY
+getMSE(onestep_arima, data_test) #ARIMA
+getMSE(onestep_ets, data_test) #ETS
+getMSE(onestep_nnar, data_test) #NNETAR
+
+getMAPE(teste, data_test) #FUZZY
+getMAPE(onestep_arima, data_test) #ARIMA
+getMAPE(onestep_ets, data_test) #ETS
+getMAPE(onestep_nnar, data_test) #NNETAR
+
